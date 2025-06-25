1330ad7cacdfb4f37b589cebe043164141df63e4
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-06-25 07:24:29 +0530

    feat: added historuu

---
+++ src/app/(about)/about-us/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AboutUsPage from '../../../pages/(About)/AboutUsPage'
+
+export default function page() {
+  return (
+   <AboutUsPage/>
+  )
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,8 @@
-import CampusGallery from '@widgets/CampusGallery'
-import Carousel from '@widgets/Carousel'
-import CounterFrame from '@widgets/CounterFrame'
-import OurCourses from '@widgets/OurCourses'
-import Welcome from '@widgets/Welcome'
+import CampusGallery from '@widgets/(Home)/CampusGallery'
+import Carousel from '@widgets/(Home)/Carousel'
+import CounterFrame from '@widgets/(Home)/CounterFrame'
+import OurCourses from '@widgets/(Home)/OurCourses'
+import Welcome from '@widgets/(Home)/Welcome'
 import React from 'react'
 
 export default function LandingPage() {
